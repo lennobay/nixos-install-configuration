f33166edb90a19848aa0c8a08a47a24e40a8cd87
lennobay <lennard.e.haufe@outlook.com>
2025-03-29 11:52:13 +0100

    formating part done

--- install-basic-uefi-formating.sh
+++ install-basic-uefi-formating.sh
@@ -4,3 +4,12 @@ parted /dev/nvme0n1 -- mkpart swap linux-swap -8GB 100%
 parted /dev/nvme0n1 -- mkpart ESP fat32 1MB 512MB
 parted /dev/nvme0n1 -- set 3 esp on
 
+mkfs.ext4 -L nixos /dev/nvme0n1p1
+mkswap -L swap /dev/nvme0n1p2
+mkfs.fat -F 32 -n boot /dev/nvme0n1p3
+
+mount /dev/disk/by-label/nixos /mnt
+mkdir -p /mnt/boot
+mount -o umask=077 /dev/disk/by-label/boot /mnt/boot
+nixos-generate-config --root /mnt
+
